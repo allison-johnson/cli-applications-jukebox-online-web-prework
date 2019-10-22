8d513299887f4beaf0bd00c7834e6f68361bc393
Allison Johnson <allison.cuttler@gmail.com>
2019-10-22 16:16:25 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -44,17 +44,6 @@ def exit_jukebox()
 end #method
 
 def run()
-  songs = [
-  "Phoenix - 1901",
-  "Tokyo Police Club - Wait Up",
-  "Sufjan Stevens - Too Much",
-  "The Naked and the Famous - Young Blood",
-  "(Far From) Home - Tiga",
-  "The Cults - Abducted",
-  "Phoenix - Consolation Prizes",
-  "Harry Chapin - Cats in the Cradle",
-  "Amos Lee - Keep It Loose, Keep It Tight"
-  ]
   puts "Please enter a command:"
   user_command = gets.strip
   while user_command != "exit" do
