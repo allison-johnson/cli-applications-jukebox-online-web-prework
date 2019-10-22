6314994537f35ae576e4f12cda5de005c78108eb
Allison Johnson <allison.cuttler@gmail.com>
2019-10-22 16:15:16 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -25,7 +25,7 @@ def play(arr)
   if arr.include?(user_choice.to_s)
     puts "Playing #{user_choice}"
   elsif user_choice.to_i < arr.length
-    puts "Playing #{arr[user_choice.to_i - 1]}"
+    puts "Playing #{arr[user_choice.to_i]}"
   else
     puts "Invalid input, please try again."
   end #if
